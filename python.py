dc1064843531767c5d554ac31cb7e6e234486a5a
yael <dote.dg.p@gmail.com>
2026-05-31 16:58:30 +0300

    python imports

---
+++ python.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, url_for, request, jsonify #שרת, HTML, אפשרות העלאה
+import random
+import os
+import numpy as np
+import requests
+import time
+from flask import request
