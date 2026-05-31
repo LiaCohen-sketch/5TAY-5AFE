071e00f2fa481b0509886c2b7a51e5b740e25009
yael <dote.dg.p@gmail.com>
2026-05-31 17:06:49 +0300

    js header changes

--- header/header.js
+++ header/header.js
@@ -1,3 +1,18 @@
-function URLcheck(){
+console.log('5tay 5afe app loaded successfully!');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
+});
+
+
+function URLhistory(){
     fetch ("/api/history");
+}
+
+function URLlearnMore(){
+    fetch ("/api/learnMore.html");
+}
+
+function URLcheck(){
+    window.location.href = "/";
 }
