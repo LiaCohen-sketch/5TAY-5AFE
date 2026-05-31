72d5c7795bc26f643f12021c02d40fd06d9f4c5d
yael <dote.dg.p@gmail.com>
2026-05-31 17:07:32 +0300

    idk

--- python.py
+++ python.py
@@ -1,9 +1,5 @@
 from flask import Flask, render_template, url_for, request, jsonify #שרת, HTML, אפשרות העלאה
-import random
-import os
-import numpy as np
-import requests
-import time
+
 from flask import request
 
 @app.rote('/')
