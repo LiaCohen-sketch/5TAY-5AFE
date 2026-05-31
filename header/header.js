676bc996cbf682c2dfde51b24dc2284a48b8ceeb
yael <dote.dg.p@gmail.com>
2026-05-31 17:02:27 +0300

    trying api

--- header/header.js
+++ header/header.js
@@ -1,3 +1,3 @@
 function URLcheck(){
-    fetch
+    fetch ("/api/history");
 }
--- python.py
+++ python.py
@@ -5,3 +5,11 @@ import numpy as np
 import requests
 import time
 from flask import request
+
+@app.rote('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/history')
+def URLcheck():
+    return render_template('history.html')
