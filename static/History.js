30a7074a93a5c28aded618fb2a0875905ec40b43
yael <dote.dg.p@gmail.com>
2026-06-01 20:43:04 +0300

    LS helpme

--- static/History.js
+++ static/History.js
@@ -1,3 +1,10 @@
+console.log('History page loaded successfully!');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
+});
+
+
 const STORAGE_KEY = "URLs";
 
 const URL = document.getElementById("URLs");
@@ -7,4 +14,20 @@ function escapeHtml(text) {
     const div = document.createElement("div");
     div.innerText = String(text);
     return div.innerHTML;
+  }
+
+  console.log(URL);
+  console.log(problems);
+
+  async function addURL(url, problems) {
+    URLs = [{
+        "url": url,
+        "problems": problems
+    }]
+  }
+
+  function loadURLs() {
+    const storedURLs = localStorage.getItem(STORAGE_KEY);
+    if (!storedURLs) return JSON.parse("");
+    URLs = JSON.parse(storedURLs);
   }
