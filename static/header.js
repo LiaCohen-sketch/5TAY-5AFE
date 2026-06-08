41c51d44db7800c3eb16c4e66a5985a06f03f26f
LiaCohen-sketch <princesslia.21911@gmail.com>
2026-06-08 20:17:13 +0300

    new header

--- static/header.js
+++ static/header.js
@@ -44,6 +44,11 @@ if (header) {
       <div class="folders" id="URLhistory">
         <img src="https://static.vecteezy.com/system/resources/thumbnails/078/371/825/small/open-yellow-file-folder-pixel-art-icon-for-data-storage-png.png" alt="URL History" onclick="URLhistory()">
         <p>URL history</p>
+        
+      </div>
+      <div class="folders" id="URLinfo">
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/078/371/825/small/open-yellow-file-folder-pixel-art-icon-for-data-storage-png.png" alt="URL Info" onclick="URLinfo()">
+        <p>Homepage</p>
       </div>
     </div>
   `;
